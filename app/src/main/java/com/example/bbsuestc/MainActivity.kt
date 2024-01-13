c9ceec4ee855236b8cd6b60308fe43924d0b33b6
cuisiwang <2528819124@qq.com>
2024-01-13 11:01:43 +0800

    登陆页面基本完成

--- app/src/main/java/com/example/bbsuestc/MainActivity.kt
+++ app/src/main/java/com/example/bbsuestc/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.LinearLayout
+import android.widget.RadioButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,33 +16,40 @@ class MainActivity : AppCompatActivity() {
         var isAutoLogin = false
         val logoLayout : LinearLayout = findViewById(R.id.login_linearlayout_with_logo)
         val loginLayout : LinearLayout = findViewById(R.id.login_linearlayout)
+        val autoLoginBt : RadioButton = findViewById(R.id.auto_login_rb)
 
-        initializeLogo(logoLayout,loginLayout)
-        logoLayout.setOnClickListener { v -> loginAnimation(logoLayout,loginLayout) }
+        val originalY : Float = logoLayout.y
 
-    }
+        initializeLogo(logoLayout,originalY)
+        logoLayout.setOnClickListener { v -> loginAnimation(logoLayout,loginLayout, originalY) }
 
-    private fun initializeLogo(logoLayout : LinearLayout, loginLayout: LinearLayout) {
-        val screenHeight: Int = resources.displayMetrics.heightPixels
-        val moveDownDistance: Float = screenHeight/2f-200
-        val moveUpAnimation: ObjectAnimator = ObjectAnimator.ofFloat(logoLayout, "translationY", 0f, moveDownDistance)
-        moveUpAnimation.duration = 0
-        val loginLayoutInvisible : ObjectAnimator = ObjectAnimator.ofFloat(loginLayout, "alpha",1f,0f)
-        loginLayoutInvisible.duration = 0
-        AnimatorSet().apply {
-            play(moveUpAnimation).with(loginLayoutInvisible)
-            start()
+        autoLoginBt.setOnClickListener {
+            if (isAutoLogin){
+                autoLoginBt.isChecked = false
+                isAutoLogin = false
+            }else{
+                autoLoginBt.isChecked = true
+                isAutoLogin = true
+            }
         }
+
     }
 
-    private fun loginAnimation(logoLayout: LinearLayout, loginLayout: LinearLayout) {
+    private fun initializeLogo(logoLayout: LinearLayout, originalY: Float) {
         val screenHeight: Int = resources.displayMetrics.heightPixels
-        val startY: Float = screenHeight/2f-200
-        val endY : Float = screenHeight /8f
-        val moveUpAnimation: ObjectAnimator = ObjectAnimator.ofFloat(logoLayout, "translationY", startY,endY)
+        val moveDownDistance: Float = screenHeight/2f-500
+        val moveDownAnimation: ObjectAnimator = ObjectAnimator.ofFloat(logoLayout, "translationY", 0f, moveDownDistance)
+        moveDownAnimation.duration = 0
+        moveDownAnimation.start()
+    }
+
+    private fun loginAnimation(logoLayout: LinearLayout, loginLayout: LinearLayout, orignialY: Float) {
+        val screenHeight: Int = resources.displayMetrics.heightPixels
+        val startY: Float = screenHeight/2f-500
+        val moveUpAnimation: ObjectAnimator = ObjectAnimator.ofFloat(logoLayout, "translationY", startY,orignialY)
         val loginLayoutVisible : ObjectAnimator = ObjectAnimator.ofFloat(loginLayout, "alpha",0f,1f)
-        loginLayoutVisible.duration = 500
-        moveUpAnimation.duration = 500 // 设置动画持续时间
+        loginLayoutVisible.duration = 400
+        moveUpAnimation.duration = 400 // 设置动画持续时间
         AnimatorSet().apply {
             loginLayout.visibility = View.VISIBLE
             play(moveUpAnimation).with(loginLayoutVisible)
