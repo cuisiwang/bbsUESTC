bd7b4226b6aa118c4e2db8154c9dd75f1a4eb9c0
cuisiwang <2528819124@qq.com>
2024-01-16 22:10:25 +0800

    登陆页面基本完成

--- app/src/main/java/com/example/bbsuestc/LoginActivityMain.kt
+++ app/src/main/java/com/example/bbsuestc/LoginActivityMain.kt
@@ -5,23 +5,28 @@ import android.animation.ObjectAnimator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.LinearLayout
 import android.widget.RadioButton
+import android.widget.TextView
 
-class MainActivity : AppCompatActivity() {
+class LoginActivityMain : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login_main)
 
         var isAutoLogin = false
         val logoLayout : LinearLayout = findViewById(R.id.login_linearlayout_with_logo)
         val loginLayout : LinearLayout = findViewById(R.id.login_linearlayout)
         val autoLoginBt : RadioButton = findViewById(R.id.auto_login_rb)
+        val loginBt : Button = findViewById(R.id.login_bt)
+        val forgetPassword : TextView = findViewById(R.id.find_password_tv)
+        val signupTv : TextView = findViewById(R.id.signup_tv)
 
         val originalY : Float = logoLayout.y
 
-        initializeLogo(logoLayout,originalY)
-        logoLayout.setOnClickListener { v -> loginAnimation(logoLayout,loginLayout, originalY) }
+        initializeLogo(logoLayout)
+        logoLayout.setOnClickListener { loginAnimation(logoLayout,loginLayout, originalY) }
 
         autoLoginBt.setOnClickListener {
             if (isAutoLogin){
@@ -33,9 +38,16 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
+        loginBt.setOnClickListener{ login() }
+
+        forgetPassword.setOnClickListener{ startForgetPasswordActivity() }
+        signupTv.setOnClickListener { startSignupActivity() }
+
     }
 
-    private fun initializeLogo(logoLayout: LinearLayout, originalY: Float) {
+    //关于加载动画，可以考虑动态改变顶部空view的大小，而不是凭空移动布局
+
+    private fun initializeLogo(logoLayout: LinearLayout) {
         val screenHeight: Int = resources.displayMetrics.heightPixels
         val moveDownDistance: Float = screenHeight/2f-500
         val moveDownAnimation: ObjectAnimator = ObjectAnimator.ofFloat(logoLayout, "translationY", 0f, moveDownDistance)
@@ -57,4 +69,15 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private fun login() {
+        TODO("Not yet implemented")
+    }
+
+    private fun startForgetPasswordActivity(){
+
+    }
+
+    private fun startSignupActivity() {
+        TODO("Not yet implemented")
+    }
 }
