55f06f3a1054a87fb15c0765f442f7e53b815da4
cuisiwang <2528819124@qq.com>
2024-01-26 15:48:41 +0800

    做了找回密码弹窗

--- app/src/main/java/com/example/bbsuestc/LoginActivityMain.kt
+++ app/src/main/java/com/example/bbsuestc/LoginActivityMain.kt
@@ -3,13 +3,15 @@ package com.example.bbsuestc
 import android.animation.AnimatorSet
 import android.animation.ObjectAnimator
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.LinearLayout
 import android.widget.RadioButton
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomsheet.BottomSheetDialog
+
 
 class LoginActivityMain : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,8 +23,7 @@ class LoginActivityMain : AppCompatActivity() {
         val loginLayout : LinearLayout = findViewById(R.id.login_linearlayout)
         val autoLoginBt : RadioButton = findViewById(R.id.auto_login_rb)
         val loginBt : Button = findViewById(R.id.login_bt)
-        val forgetPassword : TextView = findViewById(R.id.find_password_tv)
-        val signupTv : TextView = findViewById(R.id.signup_tv)
+        val forgetPassword : TextView = findViewById(R.id.find_password_tv)  //忘记密码，注册
 
         val originalY : Float = logoLayout.y
 
@@ -42,7 +43,6 @@ class LoginActivityMain : AppCompatActivity() {
         loginBt.setOnClickListener{ login() }
 
         forgetPassword.setOnClickListener{ startForgetPasswordActivity() }
-        signupTv.setOnClickListener { startSignupActivity() }
 
     }
 
@@ -73,13 +73,31 @@ class LoginActivityMain : AppCompatActivity() {
     private fun login() {
         val intent = Intent(this,HomeActivity::class.java)
         startActivity(intent)
+        finish()
     }
 
     private fun startForgetPasswordActivity(){
+        val forgetPasswordDialog : BottomSheetDialog = BottomSheetDialog(this)
+        val bottomSheetView: View =
+            layoutInflater.inflate(R.layout.dialog_home_find_password, null)
+        forgetPasswordDialog.setContentView(bottomSheetView)
 
-    }
+        val forgetPasswordTV : TextView = bottomSheetView.findViewById(R.id.forgetPasswordTv)
+        val signupTV : TextView = bottomSheetView.findViewById(R.id.signupTv)
+        val cancelTV : TextView = bottomSheetView.findViewById(R.id.cancelTv)
 
-    private fun startSignupActivity() {
-        TODO("Not yet implemented")
+        forgetPasswordTV.setOnClickListener {
+            // TODO: 启动对应activity
+        }
+
+        signupTV.setOnClickListener {
+            // TODO: 启动对应activity
+        }
+
+        cancelTV.setOnClickListener {
+            forgetPasswordDialog.dismiss()
+        }
+
+        forgetPasswordDialog.show()
     }
 }
