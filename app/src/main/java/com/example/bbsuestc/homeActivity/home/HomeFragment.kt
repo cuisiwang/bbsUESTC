f59c357504cbe33e10dccbe0ffdea28be3dd1b46
崔思惘 <2528819124@qq.com>
2024-02-02 16:31:52 +0800

    明天住院了，push一下XD

--- app/src/main/java/com/example/bbsuestc/homeActivity/home/HomeFragment.kt
+++ app/src/main/java/com/example/bbsuestc/homeActivity/home/HomeFragment.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toolbar
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.bbsuestc.R
@@ -26,6 +28,9 @@ class HomeFragment : Fragment() {
             textView.text = it  //这里的.text指的是TextView控件的text属性，可以对应的修改textSize一类的属性
             textView.textSize=it.length/1F
         }
+
+        val tb : androidx.appcompat.widget.Toolbar = root.findViewById(R.id.home_toolbar)
+        (activity as AppCompatActivity).setSupportActionBar(tb)
 
         textView.setOnClickListener { homeViewModel.changeText() }
 
