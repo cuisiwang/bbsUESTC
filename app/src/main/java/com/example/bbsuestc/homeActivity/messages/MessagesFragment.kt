971bb84402581302af97446fecc577eddf07b067
peahawf <1161428420@qq.com>
2024-02-20 18:41:28 +0800

    更改了message主界面一些相关命名，更改了样式布局。

--- app/src/main/java/com/example/bbsuestc/homeActivity/messages/MessagesFragment.kt
+++ app/src/main/java/com/example/bbsuestc/homeActivity/messages/MessagesFragment.kt
@@ -1,13 +1,11 @@
 package com.example.bbsuestc.homeActivity.messages
 
-import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
-import android.view.Menu
-import android.view.MenuInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.ImageView
+import android.widget.Spinner
 
 import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
@@ -17,11 +15,12 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.bbsuestc.R
 import com.example.bbsuestc.recyclerViewContents.MessageContent.MessageContentAdapter
 import com.example.bbsuestc.recyclerViewContents.MessageContent.MessageItem
-import com.example.bbsuestc.recyclerViewContents.postsContent.PostsItem
 
 class MessagesFragment : Fragment() {
-
-
+    private  lateinit var toolbar: Toolbar
+    private lateinit var rv:RecyclerView
+    private lateinit var message_toolbar_iv:ImageView
+    private lateinit var message_toolbar_spinner:Spinner
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -32,8 +31,10 @@ class MessagesFragment : Fragment() {
 
         val view: View = inflater.inflate(R.layout.fragment_messages,container,false)
 
-//        val toolbar:Toolbar=view.findViewById(R.id.toolbar_message) TODO: 参考别的类，将控件声明独立到方法外
-//        val rv:RecyclerView=view.findViewById(R.id.friendRv)
+        toolbar=view.findViewById(R.id.toolbar_message)
+        rv=view.findViewById(R.id.message_interact_rv)
+        message_toolbar_iv=view.findViewById(R.id.message_toolbar_add_iv)
+        message_toolbar_spinner=view.findViewById(R.id.message_toolbar_spinner)
         //做一个数据
         val data = arrayListOf<MessageItem>()
         for(i in 0..130){
@@ -41,6 +42,8 @@ class MessagesFragment : Fragment() {
         }
         rv.adapter= MessageContentAdapter(data)
         rv.layoutManager=LinearLayoutManager(activity)
+        //点击图片，激活spinner
+        message_toolbar_iv.setOnClickListener{message_toolbar_spinner.performClick()}
 //        toolbar.inflateMenu(R.menu.message_menu)  TODO：使用spinner
 //        toolbar.overflowIcon = resources.getDrawable(R.drawable.ic_add)
         return view
