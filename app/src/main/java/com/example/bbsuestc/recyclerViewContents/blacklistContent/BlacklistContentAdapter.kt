d9a455fe3e4fa78c8802f8677260173344bf8e02
cuisiwang <2528819124@qq.com>
2024-04-04 18:40:43 +0800

    code merged 请以此分支为准！！！

--- app/src/main/java/com/example/bbsuestc/recyclerViewContents/blacklistContent/BlacklistContentAdapter.kt
+++ app/src/main/java/com/example/bbsuestc/recyclerViewContents/blacklistContent/BlacklistContentAdapter.kt
@@ -9,53 +9,57 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.bbsuestc.R
 
-class BlackListContentAdapter(private val data:ArrayList<BlackListItems>, private val context: Context)
-    : RecyclerView.Adapter<BlackListContentAdapter.ViewHolder>() {
+class BlackListContentAdapter(
+    private val data: ArrayList<BlackListItems>?,
+    private val context: Context
+) : RecyclerView.Adapter<BlackListContentAdapter.ViewHolder>() {
 
-    interface OnOptionClickListener{
+    interface OnOptionClickListener {
         fun onOptionItemClick(position: Int)
     }
-    private lateinit var mOnclickListener:OnOptionClickListener
-    fun setOnOptionClickListener(onclickListener: OnOptionClickListener){
-        mOnclickListener=onclickListener
+
+    private lateinit var mOnclickListener: OnOptionClickListener
+    fun setOnOptionClickListener(onclickListener: OnOptionClickListener) {
+        mOnclickListener = onclickListener
     }
 
-    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
-        var blacklistUserIcon:ImageView
-        var blacklistUserName:TextView
-        var blacklistOption:ImageView
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var blacklistUserIcon: ImageView
+        var blacklistUserName: TextView
+        var blacklistOption: ImageView
+
         init {
-            blacklistUserIcon=itemView.findViewById(R.id.blacklist_userIcon_iv)
-            blacklistUserName=itemView.findViewById(R.id.blacklist_userName_tv)
-            blacklistOption=itemView.findViewById(R.id.blacklist_option_iv)
+            blacklistUserIcon = itemView.findViewById(R.id.blacklist_userIcon_iv)
+            blacklistUserName = itemView.findViewById(R.id.blacklist_userName_tv)
+            blacklistOption = itemView.findViewById(R.id.blacklist_option_iv)
 
         }
-        public fun setData(position: Int){
-            blacklistUserName.text = data[position].blacklistUserName
+
+        public fun setData(position: Int) {
+            blacklistUserName.text = data?.get(position)?.blacklistUserName ?: "默认用户"
             //TODO:设置头像
         }
     }
+
     override fun onCreateViewHolder(
         parent: ViewGroup,
         viewType: Int
     ): BlackListContentAdapter.ViewHolder {
-        val view:View=LayoutInflater.from(context).inflate(R.layout.item_blacklist_content,parent,false)
+        val view: View =
+            LayoutInflater.from(context).inflate(R.layout.item_blacklist_content, parent, false)
         return ViewHolder(view)
     }
 
 
-
     override fun onBindViewHolder(holder: BlackListContentAdapter.ViewHolder, position: Int) {
         holder.setData(position)
-        holder.blacklistOption.setOnClickListener{
-            if(mOnclickListener!=null){
-                mOnclickListener.onOptionItemClick(position)
-            }
+        holder.blacklistOption.setOnClickListener {
+            mOnclickListener.onOptionItemClick(position)
         }
     }
 
     override fun getItemCount(): Int {
-        return data.size
+        return data?.size ?: 0
     }
 
 
