aeb1f25c8b964aa20bc97c79acc940c086eeb1a8
cuisiwang <2528819124@qq.com>
2024-04-04 20:44:01 +0800

    3

---
+++ app/src/main/java/com/example/bbsuestc/blackListActivity/BlackListViewModel.kt
@@ -0,0 +1,24 @@
+package com.example.bbsuestc.blackListActivity
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.bbsuestc.recyclerViewContents.blackListContent.BlackListItems
+
+class BlackListViewModel : ViewModel() {
+    private val _blackListLiveData = MutableLiveData<ArrayList<BlackListItems>>().apply {
+        value = initBlackList()
+    }
+
+    val blackList: LiveData<ArrayList<BlackListItems>> = _blackListLiveData;
+
+    private fun initBlackList(): ArrayList<BlackListItems> {
+        val list = arrayListOf<BlackListItems>()
+        for (i in 1..50) {
+            list.add(BlackListItems("", "河畔用户$i"))
+        }
+        return list
+    }
+
+
+}
