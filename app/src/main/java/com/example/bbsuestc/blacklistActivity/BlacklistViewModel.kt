cbb1c7ea0c110c81c797090701ac6e1591d3b665
cuisiwang <2528819124@qq.com>
2024-04-04 20:38:08 +0800

    1

--- app/src/main/java/com/example/bbsuestc/blacklistActivity/BlacklistViewModel.kt
+++ app/src/main/java/com/example/bbsuestc/blacklistActivity/BlacklistViewModel.kt
@@ -4,7 +4,6 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.bbsuestc.recyclerViewContents.blackListContent.BlackListItems
-import com.example.bbsuestc.recyclerViewContents.messageContent.MessageItem
 
 class BlackListViewModel : ViewModel() {
     private val _blackListLiveData = MutableLiveData<ArrayList<BlackListItems>>().apply {
@@ -13,7 +12,7 @@ class BlackListViewModel : ViewModel() {
 
     val blackList: LiveData<ArrayList<BlackListItems>> = _blackListLiveData
 
-    private fun initBlackList(): ArrayList<BlackListItems>? {
+    private fun initBlackList(): ArrayList<BlackListItems> {
         val list = arrayListOf<BlackListItems>()
         for (i in 1..50) {
             list.add(BlackListItems("", "河畔用户$i"))
